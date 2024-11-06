86ffe6b83f0a6e2d8ddb34a85cf5fd9d3bd9a208
joselm00 <joseluismartinezsaez879@gmail.com>
2024-11-06 16:41:19 +0100

    Creacion constructor Direccion

--- src/main/java/org/iesalandalus/programacion/damas/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/damas/Direccion.java
@@ -4,4 +4,8 @@ public enum Direccion {
     NORESTE("Noreste"),SURESTE("Sureste"),NOROESTE("Noroeste"),SUROESTE("Suroeste");
 
     private String cadenaAMostrar;
+    private Direccion(String cadenaAMostrar){
+        this.cadenaAMostrar=cadenaAMostrar;
+    }
+
 }
