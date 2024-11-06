9b1be8fb9fbbea314209bc5781fe27f2a97d8f55
joselm00 <joseluismartinezsaez879@gmail.com>
2024-11-06 18:10:23 +0100

    Creacion de los getters y setters de columna y fila clase Posicion

--- src/main/java/org/iesalandalus/programacion/damas/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/Posicion.java
@@ -4,4 +4,27 @@ public class Posicion {
 
     int fila;
     char columna;
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna<'a'||columna>'h'){
+            throw new IllegalArgumentException("ERROR, valor incorrecto, vuelva a introducirlo");
+        }
+        this.columna=columna;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila<1 || fila>8){
+            throw new IllegalArgumentException("ERROR, valor incorrecto, vuelva a introducirlo");
+        }
+        this.fila=fila;
+    }
+
 }
