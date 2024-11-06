8b6cb5f84fdee122292ebf39656861b17a2727ce
joselm00 <joseluismartinezsaez879@gmail.com>
2024-11-06 16:38:27 +0100

    Creacion atributo cadenaAMostrar

--- src/main/java/org/iesalandalus/programacion/damas/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/damas/Direccion.java
@@ -3,8 +3,5 @@ package org.iesalandalus.programacion.damas;
 public enum Direccion {
     NORESTE("Noreste"),SURESTE("Sureste"),NOROESTE("Noroeste"),SUROESTE("Suroeste");
 
-    private String Cadena;
-    private Direccion(String Cadena){
-        this.Cadena=Cadena;
-    }
+    private String cadenaAMostrar;
 }
