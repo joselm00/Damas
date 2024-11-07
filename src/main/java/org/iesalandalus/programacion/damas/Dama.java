f7810439b5aa048044ac67a65477a1f9f865defc
joselm00 <joseluismartinezsaez879@gmail.com>
2024-11-07 13:15:04 +0100

    Creacion de los setters y los getters de los atributos

--- src/main/java/org/iesalandalus/programacion/damas/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/Dama.java
@@ -6,5 +6,34 @@ public class Dama {
         private Posicion posicion;
         boolean esDamaEspecial;
 
+    public Color getColor() {
+        return color;
+    }
 
+    public void setColor(Color color) {
+
+        if (color==null){
+            throw new IllegalArgumentException("Error, el color no puede ser nulo");
+        }
+        this.color = color;
+    }
+
+    public boolean isEsDamaEspecial() {
+        return esDamaEspecial;
+    }
+
+    public void setEsDamaEspecial(boolean esDamaEspecial) {
+        this.esDamaEspecial = esDamaEspecial;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        if (posicion==null){
+            throw new IllegalArgumentException("Error, la posición no puede ser nula");
+        }
+        this.posicion = posicion;
+    }
 }
