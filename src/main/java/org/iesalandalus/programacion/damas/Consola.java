d71bbb9c532cb80f47afcfca921f6f9d2687bd16
joselm00 <joseluismartinezsaez879@gmail.com>
2024-11-08 14:19:24 +0100

    Creacion del metodo elegirOpcionMenu

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -1,6 +1,10 @@
 package org.iesalandalus.programacion.damas;
 
+import java.util.Scanner;
+
 public class Consola {
+
+    private static final Scanner entrada = new Scanner(System.in);
 
     private Consola() {
 
@@ -9,13 +13,25 @@ public class Consola {
     public static void mostrarMenu(){
         System.out.println("Menú de opciones: ");
         System.out.println("==============================================");
-        System.out.println("Crear dama por defecto");
-        System.out.println("Crear dama eligiendo color");
-        System.out.println("Mover dama");
-        System.out.println("Salir");
+        System.out.println("1.Crear dama por defecto");
+        System.out.println("2.Crear dama eligiendo color");
+        System.out.println("3.Mover dama");
+        System.out.println("4.Salir");
     }
 
-
+    public static int  elegirOpcionMenu(){
+    int opcion = 0;
+    do {
+        System.out.println("Introduce por teclado una opcion del menú de opciones(1-4): ");
+            while (!entrada.hasNextInt()){
+                System.out.println("ERROR, debes ingresar un numero entre 1 y 4");
+                entrada.next();
+                System.out.println("Introduce por teclado una opcion del menú de opciones(1-4): ");
+        }
+            opcion= entrada.nextInt();
+    }while (opcion<1||opcion>4);
+        return opcion;
+    }
 
 
 }
