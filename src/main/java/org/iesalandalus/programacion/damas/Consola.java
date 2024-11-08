9b4e2e0e43a8852299bb734a68e0188268c39698
joselm00 <joseluismartinezsaez879@gmail.com>
2024-11-08 14:08:04 +0100

    Creacion del metodo mostrarMenu

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -6,4 +6,16 @@ public class Consola {
 
     }
 
+    public static void mostrarMenu(){
+        System.out.println("Menú de opciones: ");
+        System.out.println("==============================================");
+        System.out.println("Crear dama por defecto");
+        System.out.println("Crear dama eligiendo color");
+        System.out.println("Mover dama");
+        System.out.println("Salir");
+    }
+
+
+
+
 }
