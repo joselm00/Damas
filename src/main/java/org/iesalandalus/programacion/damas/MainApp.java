c100f01a1148d5bce8e2f484269f4734bb422d09
joselm00 <joseluismartinezsaez879@gmail.com>
2024-11-12 12:20:06 +0100

    Creacion del metodo ejecutarOpcion en el Mainapp

--- src/main/java/org/iesalandalus/programacion/damas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -7,5 +7,31 @@ public class MainApp {
 
     public static Dama dama;
 
+    private static void ejecutarOpcion(int opcion){
+
+        switch (opcion){
+
+            case 1 -> {
+                System.out.println("Crear dama por defecto");
+            }
+            case 2 -> {
+                System.out.println("Nuevo dama color");
+            }
+            case 3 -> {
+                System.out.println("Mover dama");
+            }
+            case 4 -> {
+                System.out.println("Mostrar dama");
+            }
+            case 5 -> {
+                System.out.println("Despedida");
+            }
+            default -> {System.out.println("Opcion no valida");}
+
+        }
+    }
+
+
+
 
 }
